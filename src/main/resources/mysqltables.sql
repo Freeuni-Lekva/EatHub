a70fb2de74ebd933eb65566788bf041e12c5d83e
Liquidibrium <vanoganjelashvili@gmail.com>
2021-07-14 18:26:37 +0400

    removed constraint names

--- src/main/resources/mysqltables.sql
+++ src/main/resources/mysqltables.sql
@@ -5,14 +5,19 @@ DROP TABLE IF EXISTS users;
 
 CREATE TABLE users
 (
-    user_id  BIGINT  NOT NULL AUTO_INCREMENT,
-    username VARCHAR(64) NOT NULL,
+    user_id  BIGINT       NOT NULL AUTO_INCREMENT,
+    username VARCHAR(64)  NOT NULL,
     password VARCHAR(256) NOT NULL,
-    email    VARCHAR(64) NOT NULL,
-    balance  DECIMAL NOT NULL,
-    role     VARCHAR(64) NOT NULL,
+    email    VARCHAR(64)  NOT NULL,
+    balance  DECIMAL      NOT NULL,
+    role     VARCHAR(64)  NOT NULL,
     PRIMARY KEY (user_id),
-    CONSTRAINT UQ_user_id UNIQUE (user_id),
-    CONSTRAINT UQ_username UNIQUE (username),
-    CONSTRAINT UQ_email UNIQUE (email)
+    UNIQUE (user_id),
+    UNIQUE (username),
+    UNIQUE (email)
 );
+
+INSERT INTO users(username, password,
+                  email, balance, role)
+VALUES ("admin", "$2a$10$.gSvGvCf5I85vxP4dklNSuqkwnFGNZxE4S04Dy6aZX76btLZic6Wm",
+        "eathub.freeuni@gmail.com", 1000, "ADMIN");
